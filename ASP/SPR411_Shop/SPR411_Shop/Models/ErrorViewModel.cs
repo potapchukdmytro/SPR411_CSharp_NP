60a1e4d78a9e868c4142ae5f38a7eba4b5a1c0ed
potapchukdmytro <traiget@gmail.com>
2026-02-14 09:41:27 +0200

    Remove asp

--- ASP/SPR411_Shop/SPR411_Shop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SPR411_Shop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
